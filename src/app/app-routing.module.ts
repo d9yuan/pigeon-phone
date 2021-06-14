2397f36cdbbf00b95577c2fcd70b0cf0744edb16
Adam Yuan <adam.y0@outlook.com>
2021-06-13 20:38:49 -0400

    first commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,10 @@
 import { NgModule } from '@angular/core';
+import { MainContentComponent } from './main-content/main-content.component';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path : '', component: MainContentComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
